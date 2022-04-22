fd1e88b7e69c35744e42f345b9621b25e989f1c9
Rafael <raphaelsantosajs@gmail.com>
2022-04-21 23:24:08 -0300

    Final Version

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+const copyBtn = document.getElementById("copy")
+
+copyBtn.addEventListener("click", () => {
+    if (copyBtn.textContent != "Copied!") copyBtn.innerHTML = "Copied!"
+
+    /* Get the text field */
+    var copyText = document.getElementById("code")
+
+    /* Select the text field */
+    copyText.select()
+    copyText.setSelectionRange(0, 99999) /* For mobile devices */
+
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value)
+})
+
 const colorRGB = () => {
     let R = document.getElementById("R").value,
         G = document.getElementById("G").value,
         B = document.getElementById("B").value
-    
+
     let color = `rgb(${R}, ${G}, ${B})`
     document.getElementById("resultColor").style.backgroundColor = color
-    document.getElementById("code").innerHTML = "background-color: " + color
+    document.getElementById("code").value = "background-color: " + color
 }
 
 document.getElementById("R").addEventListener("input", colorRGB)
